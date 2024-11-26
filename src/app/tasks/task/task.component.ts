74fa9f2daaf31eef2c8fb61f1dd8da295ed31f47
MycaelaRamona <devramonamartins@gmail.com>
2024-11-26 10:26:39 +0000

    creation of the shared Card and Service.

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,19 +1,28 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
+
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>(); //por isso tenho <string> devido ao evento que emito!
+  //@Output() complete = new EventEmitter<string>(); //por isso tenho <string> devido ao evento que emito!
+
+  private tasksService = inject(TasksService);
 
   onCompleteTask(){
-    this.complete.emit(this.task.id); //se aqui estou emitindo um evento, eu coloco o tipo desse evento no meu            EventEmitter<string>
+    //this.complete.emit(this.task.id); //se aqui estou emitindo um evento, eu coloco o tipo desse evento no meu            EventEmitter<string>
+
+    //Aqui eu usso o metodo criado no serviço:
+    this.tasksService.removeTask(this.task.id);
   }
 
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,45 +1,38 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input() userId!: string;
   @Input() name!: string;
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31'
-    },
-    {
-      id: 't2',
-      userId: 'u2',
-      title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31'
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31'
-    }
-  ];
+
+  isAddingTask = false;
+
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onCompleteTask(id: string){
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+
   }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask(){
+    this.isAddingTask = false;
+  }
+
 }
