2f507b6386019d42f594ad973a1b3db8ba8a5995
MycaelaRamona <devramonamartins@gmail.com>
2024-11-26 17:03:13 +0000

    Creation of the Modules Tasks and Share.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DatePipe } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { SharedModule } from './shared/shered.module';
+import { TasksModule } from './tasks/tasks.module';
+
 
 @NgModule({
-  declarations: [AppComponent], //aqui eu só declaro componentes não standalone!
+  //aqui eu só declaro componentes não standalone!:
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent
+  ],
   bootstrap: [AppComponent],
   //Aqui posso importar componentes autônomos (standalone)
   //Nota: Todo aplicativo ao usar o Angular Module precisa usar o BrowserModule.
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent]
+  imports: [
+    BrowserModule,
+    DatePipe, //Não necessáriamente preciso declara O DatePipe aqui pois ele faz parte do paconte BrowserModule
+    SharedModule,
+    TasksModule
+  ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/shared/shered.module.ts
@@ -0,0 +1,9 @@
+import { CardComponent } from './card/card.component';
+import { NgModule } from "@angular/core";
+
+
+@NgModule({
+  declarations: [CardComponent],
+  exports: [CardComponent]
+})
+export class SharedModule {}
